b958a0a92ffe79676221c83ba9295b38be9fb9d6
TheodorStolle <theodorfelixstolle@gmail.com>
2022-05-15 13:29:36 +0200

    Second step

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var app = angular.module('TodoList', []);
+var app = angular.module("TodoList", []);
 
-app.controller('MainCtrl', [
+app.controller("MainCtrl", [
 '$scope',
 function($scope){
-  $scope.test = 'TodoList HA02';
+  $scope.test = "TodoList HA02";
 
   $scope.todos = [
-      {name: 'Wäsche waschen', Date: '2022-05-15' , progress: '30%'},
-      {name: 'Hausaufabe beenden', Date: '2022-05-19' , progress: 30},
-      {name: 'Präsentaton vorbereiten', Date: '2022-06-10' , progress: 30},
+      {name: "Wäsche waschen", Date: "2022-05-15" , progress: "30%"},
+      {name: 'Hausaufabe beenden', Date: '2022-05-19' , progress: "30%"},
+      {name: "Präsentaton vorbereiten", Date: "2022-06-10" , progress: "30%"},
   ];
+
+  $scope.addTodo = function(){
+    $scope.todos.push({name: "A new Post", Date: "2024-11-11" , progress:"40%"});
+  };
+
 }]);
