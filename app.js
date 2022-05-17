dd2094cf58189093488864334bf3c308dd02e13e
n7klas <104858036+n7klas@users.noreply.github.com>
2022-05-17 15:15:21 +0200

    sorted

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function($scope){
 
   $scope.todos = [
       {name: "Wäsche waschen", Date: "2022-05-15" , progress: "30%"},
-      {name: 'Hausaufabe beenden', Date: '2022-05-19' , progress: "30%"},
+      {name: "Hausaufabe beenden", Date: "2022-05-19" , progress: "30%"},
       {name: "Präsentaton vorbereiten", Date: "2022-06-10" , progress: "30%"},
   ];
 
